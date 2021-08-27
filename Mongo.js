cb8f8cda594b9701ce06e8dad6850e717fc49a98
Dmitrii <nebaz@ya.ru>
2021-08-27 18:04:57 +0300

    update mongo driver to v4, add createIndex function

--- Mongo.js
+++ Mongo.js
@@ -3,11 +3,15 @@ const ObjectId = require('mongodb').ObjectId;
 
 class Mongo {
 
+  constructor() {
+    this.ObjectId = ObjectId;
+  }
+
   async connect(config) {
     this.config = config;
     let client = await MongoClient.connect(config.connectionString, config.options);
     for (let db of config.dbs) {
-      if (!this[db] || !this[db].serverConfig.isConnected()) {
+      if (!this[db]) {
         this[db] = client.db(db);
       }
     }
@@ -16,7 +20,7 @@ class Mongo {
   getCollection(dbName, collectionName) {
     dbName = dbName.toString();
     collectionName = collectionName.toString();
-    if (!this[dbName] || !this[dbName].serverConfig.isConnected()) {
+    if (!this[dbName]) {
       throw new Error('mongo connect error');
     }
     let collection = this[dbName].collection(collectionName);
@@ -35,11 +39,28 @@ class Mongo {
 
   async getCollectionNames(dbName) {
     dbName = dbName.toString();
-    if (!this[dbName] || !this[dbName].serverConfig.isConnected()) {
+    if (!this[dbName]) {
       throw new Error('mongo connect error');
     }
     let collections = await this[dbName].listCollections().toArray();
     return collections.map(item => item.name);
+  }
+
+  async createIndex(dbName, collectionName, indexName, fields, options = {}) {
+    let collection = this.getCollection(dbName, collectionName);
+    let indexes = await this.getIndexesByCollection(collection);
+    if (!indexes.includes(indexName)) {
+      await collection.createIndex(fields, options);
+    }
+  }
+
+  async getIndexesByCollection(collection) {
+    try {
+      let indexes = await collection.indexes();
+      return indexes.map(it => Object.keys(it.key).join());
+    } catch (e) {
+      return [];
+    }
   }
 
   async getNextCounterByName(name) {
